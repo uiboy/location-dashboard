0fef15158ab3f7202332170f1dfd9e9ead7a4a23
Shantanu <shantanutarey@gmail.com>
2020-06-12 16:53:31 +0200

    :wrench: package json, other config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': ['error', { singleQuote: true, printWidth: 125 }]
   },
   overrides: [
     {
